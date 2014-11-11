e2040749961c7d1a6f1cc8fc452d5477b39eeaa3
Josh King <kingjoshdavid@gmail.com>
2014-11-11 09:15:32 -0800

    dangerous methods used here.

--- explore/js/clock.jsx
+++ explore/js/clock.jsx
@@ -1,10 +1,18 @@
-/*global define*/
+/*global define, window*/
 
 define(['react'], function (React) {
     return React.createClass({
-
+        getInitialState: function () {
+            return {time: "Now"}
+        },
+        updateTime: function () {
+            this.setState({time: "<em>"+new Date() + "<em>"});
+        },
+        componentDidMount: function () {
+            window.setInterval(this.updateTime, 100);
+        },
         render: function () {
-            return <div> Time is <span> {this.state.time}</span></div>;
+            return <div> Time is <span dangerouslySetInnerHTML={{__html: this.state.time}} /></div>;
         }
     });
 });
