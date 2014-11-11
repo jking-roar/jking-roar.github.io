c7d3aa66be0a55823ad40500c2438b606f83fba6
Josh King <kingjoshdavid@gmail.com>
2014-11-11 09:09:29 -0800

    ignore idea files

--- explore/js/clock.jsx
+++ explore/js/clock.jsx
@@ -1,10 +1,10 @@
+/*global define*/
+
 define(['react'], function (React) {
     return React.createClass({
 
         render: function () {
-            return <div> Time
-            is <span>{this.state.time}</span></div >
-}
+            return <div> Time is <span> {this.state.time}</span></div>;
+        }
+    });
 });
-})
-;
